83e91809b744bc78d0ad56394f8145a02077a4ef
Patrick McDavid <patrick@bombbomb.com>
2016-09-23 11:15:05 -0600

    jwt auth, staged bucket

--- handler.js
+++ handler.js
@@ -3,24 +3,73 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
-var bucket = 'userfiles.bombbomb.com';
+function genBucketName(stage) {
+    return stage + ".userfiles.bombbomb.com";
+}
 
 module.exports.list = function (event, context, cb) {
-
-    s3.listObjects({Bucket: bucket}, function(err, data){
+    console.log(event);
+    s3.listObjects({Bucket: genBucketName(event.stage)}, function(err, data){
         console.log(err, data);
         cb(null, data);
     });
-
 };
 
 module.exports.sign = function (event, context, cb) {
 
-    var userId = 'candy';
+    console.log(event);
 
-    var params = {Bucket: bucket, Key: userId + '/' + event.path.filename};
+    var key = event.principalId + '/' + event.path.filename;
+
+    var params = {
+        Bucket: genBucketName(event.stage),
+        ACL: 'public-read',
+        Key: key
+    };
+
+    console.log(params);
+
     s3.getSignedUrl('putObject', params, function (err, url) {
+        console.log(err, url);
         cb(null, url);
     });
+};
+
+
+module.exports.auth = function (event, context, cb) {
+
+    console.log(event);
+
+    var jwt = require('jsonwebtoken');
+    var fs = require('fs');
+    var cert = fs.readFileSync('public.pem');
+
+    jwt.verify(event.authorizationToken, cert, { audience: 'BBApp' }, function(err, decoded) {
+        console.log(err, decoded);
+
+        if (err) {
+            context.fail("Unauthorized");
+        } else {
+            context.succeed(generatePolicy(decoded.sub, 'Allow', event.methodArn));
+        }
+
+    });
+
+    var generatePolicy = function(principalId, effect, resource) {
+        var authResponse = {};
+        authResponse.principalId = principalId;
+        if (effect && resource) {
+            var policyDocument = {};
+            policyDocument.Version = '2012-10-17'; // default version
+            policyDocument.Statement = [];
+            var statementOne = {};
+            statementOne.Action = 'execute-api:Invoke'; // default action
+            statementOne.Effect = effect;
+            statementOne.Resource = resource;
+            policyDocument.Statement[0] = statementOne;
+            authResponse.policyDocument = policyDocument;
+        }
+        return authResponse;
+    }
 
 };
